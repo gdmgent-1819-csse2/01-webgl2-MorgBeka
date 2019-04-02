09650dbede171c184e0af99090ac5094bbae23a1
MorgBeka <morgbeka@student.arteveldehs.be>
2019-04-02 17:32:13 +0200

    update clean code

--- 03-MorgBeka-v.0.3.0/assets/js/main.js
+++ 03-MorgBeka-v.0.3.0/assets/js/main.js
@@ -1,3 +1,11 @@
+/**********************
+ * @author      Parent Olivier, Morgane Bekaert
+ * @created     27/02/'19
+ * @modified    2/04/'19
+ * @copyright   Copyright © 2017-2018 Areteveldehogeschool
+ * @function    Canvas - Drawing clock 
+ ************************/
+
 import Application from './Application.js'
 
 window.addEventListener('DOMContentLoaded', _ => new Application(), false)
